dd2d5d839d39aee3282feac2e0b0183004a632d7
judenoodles <wandajdlc.528@gmail.com>
2024-03-05 21:46:55 +0800

    still editing

---
+++ tempBusFull.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    char seats13[5][3];
+    
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            seats13[i][j] = 'O';
+        }
+    }
+    
+    seats13[4][1] = '\0';
+    seats13[4][2] = '\0';
+    
+    printf("13 - Seater Bus: All Vacant\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%c  ", seats13[i][j]);
+        }
+        printf("\n");
+    }
+    
+    char seats16[4][4];
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            seats16[i][j] = 'O';
+        }
+    }
+
+    printf("\n16 - Seater Bus: All Vacant\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%c  ", seats16[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+    return 0;
+}
